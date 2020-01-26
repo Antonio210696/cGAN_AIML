2240292cdb05a077d14920f71346b8a3b536f760
antonio <antonio216996@gmail.com>
2020-01-26 21:07:09 +0100

    modified generator again

--- cGAN.py
+++ cGAN.py
@@ -53,8 +53,11 @@ class Generator(nn.Module):
             nn.Sigmoid(),
 			Reshape(batch_size, 80, 10, 10), 
 			nn.BatchNorm2d(80),
+			nn.Relu(),
 			nn.ConvTranspose2d(80, 30, 3, 1, bias=False),
+			nn.Relu(),
             nn.ConvTranspose2d(30, 3, 20, 4, bias=False)
+			nn.Relu()
             # nn.Linear(self.depth * 8, int(np.prod(img_shape))), # np.prod ritorna il prodotto dei valori sugli axes - in questo caso il prodotto delle dimensioni dell'immagine
             # nn.Tanh()    
 			)
