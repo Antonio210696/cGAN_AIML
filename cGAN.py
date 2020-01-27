8c5fb5f893bd89d9cbc06d6bed932401d7b0874f
antonio <antonio216996@gmail.com>
2020-01-27 23:45:44 +0100

    added some layers

--- cGAN.py
+++ cGAN.py
@@ -114,6 +114,7 @@ class Discriminator(nn.Module):
         )
         self.main = nn.Sequential(
             # input is (nc) x 64 x 64
+            # boh
             nn.Conv2d(nc, ndf, 4, 2, 1, bias=False),
             nn.LeakyReLU(0.2, inplace=True),
             # state size. (ndf) x 32 x 32
